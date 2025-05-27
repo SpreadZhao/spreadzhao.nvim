41a1172143f1066d8057de5ae92dc89f35c6e629
SpreadZhao <spreadzhao@outlook.com>
2025-05-28 02:27:38 +0800

    Start using fzf-lua

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,17 @@
+return {
+  'ibhagwan/fzf-lua',
+  -- optional for icon support
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "echasnovski/mini.icons" },
+  keys = {
+    {
+      '<leader>ff',
+      function()
+        require('fzf-lua').files()
+      end,
+      mode = 'n',
+      desc = '[F]ind [F]iles',
+    },
+  },
+}
